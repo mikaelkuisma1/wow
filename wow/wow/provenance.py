fa8ecb55f9f5c25a99b7679e090484def9db92d1
Mikael Kuisma <kuisma@dtu.dk>
2026-06-11 23:21:45 +0200

    Start building rdf descriptors. Created unorthodox 2-tuple PredicateObjectTuple, because that allows nice syntax wow.dependsOn(wow.task) which will be used for dataclass fields

--- wow/wow/provenance.py
+++ wow/wow/provenance.py
@@ -3,13 +3,17 @@ from dataclasses import dataclass
 class Namespace:
     def __init__(self, iri: str, prefix: str | None = None):
         self.iri = iri
-        self.prefix = prefix
+        self._prefix = prefix
     
     def __getattr__(self, attr: str):
         return Term(self, attr)
 
     def __repr__(self):
-        return self.iri
+        return f'Namespace("{self.iri}")'
+
+    @property
+    def prefix(self):
+        return self._prefix or self.iri
 
 @dataclass(frozen=True)
 class Term:
@@ -18,8 +22,21 @@ class Term:
 
     @property
     def iri(self):
-        return self.namespace.iri + self.name
+        return self.namespace.iri + ':' + self.name
 
+    def __call__(self, obj: Term):
+        return PredicateObjectTuple(self, obj)
+
+    def __repr__(self):
+        return self.namespace.prefix + ':' + self.name 
+
+@dataclass(frozen=True)
+class PredicateObjectTuple:
+    predicate: Term
+    obj: Term
+
+    def __repr__(self):
+        return f'pred:{self.predicate} obj:{self.obj}'
 
 def rdfclass(*, type=None):
     """Class decorator for dataclass like structure, but for with rdf
@@ -33,3 +50,8 @@ def rdfclass(*, type=None):
 if __name__ == "__main__":
     wow = Namespace('workflowofworkflows.example.com', 'wowdemo')
     print(wow)
+    print(wow.task)
+    print(wow.dependsOn(wow.task))
+    #@rdfclass(type=wow._
+    #class WorkflowDescription:
+
